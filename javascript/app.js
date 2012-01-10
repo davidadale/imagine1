fce79fe39e4939e0a270d20894448e48c3bd6bd9
david dale <david@flow7.net>
2012-01-09 23:47:33 -0500

    updated with new look and feel

---
+++ javascript/app.js
@@ -0,0 +1,45 @@
+$(function(){
+
+    window.AppView = Backbone.View.extend({
+        el:$("#center"),
+        homeTemplate: _.template($("#home").html() ),
+        envisionTemplate:_.template($("#envision").html()),
+        embraceTemplate:_.template($("#embrace").html()),
+        joinTemplate:_.template($("#join").html()),
+        engageTemplate:_.template($("#engage").html()),
+        promoteTemplate:_.template($("#promote").html()),
+        joinFormTemplate:_.template($("#joinForm").html()),
+        
+        initialize: function(){
+            //this.el.html( this.homeTemplate );
+        },
+        envision:function(){
+            this.el.html( this.envisionTemplate );
+        },
+        embrace:function(){
+            this.el.html( this.embraceTemplate );
+        },
+        join:function(){
+            this.el.html( this.joinTemplate );
+        },
+        engage:function(){
+            this.el.html( this.engageTemplate );
+        },        
+        promote:function(){
+            this.el.html( this.promoteTemplate );
+        },
+        home:function(){
+            this.el.html( this.homeTemplate );
+        },
+        joinForm:function(){
+            this.el.html( this.joinFormTemplate );
+        }
+        
+    });
+    
+    window.App = new AppView;
+    
+    App.home();
+    
+    //AppView.render();
+})
