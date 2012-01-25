daed9ab8d14fc638ab4931c4ffe1eed7c931aac2
david dale <david@flow7.net>
2012-01-24 22:18:53 -0500

    adding url

--- javascript/app.js
+++ javascript/app.js
@@ -69,7 +69,7 @@ $(function(){
         addOne: function(member) {
             //console.log("ding ding ding: " + member[0]);     
              console.log( JSON.stringify( member ) );      
-            $("#membersList").append( "<li>" + member.name[0] + "</li>" );
+            $("#membersList").append( "<li>" + member.get("name") + "</li>" );
         },
         addAll: function(){
             console.log("bang bang bang");
